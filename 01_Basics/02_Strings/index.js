0a5b2c9a5976d3e212e7f32fa0d4178b4f24a50d
TiagoB67 <tiagobiernath@gmail.com>
2025-11-05 16:18:55 +0100

    02_Strings/Done

--- 01_Basics/02_Strings/index.js
+++ 01_Basics/02_Strings/index.js
@@ -1,1 +1,11 @@
-console.log("Tiago is Batman");
+let a = 17.4;
+let b = 3.9;
+
+let result = a / b;
+
+
+let firstName = "Tiago";
+let lastName = "Biernath";
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+console.log("Ich heiße " + firstName + " " + lastName);
