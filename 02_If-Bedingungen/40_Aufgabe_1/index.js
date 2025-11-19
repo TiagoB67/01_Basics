8c98d163908c44943db0f319e72440ea3cbdcd27
TiagoB67 <tiagobiernath@gmail.com>
2025-11-19 15:23:46 +0100

    40_If-Bedingungen_Aufgabe_1 Done

--- 02_If-Bedingungen/40_Aufgabe_1/index.js
+++ 02_If-Bedingungen/40_Aufgabe_1/index.js
@@ -1,7 +1,5 @@
 let a = 10;
-let b = -10;
-let c = 0;
 
 if (a > 0) {console.log ("The Number is Positive");}
-if (b < 0 ) {console.log ("The Number is Negative");}
-if (c == 0) {console.log ("The Number is Zero");}
+if (a < 0 ) {console.log ("The Number is Negative");}
+if (a == 0) {console.log ("The Number is Zero");}
