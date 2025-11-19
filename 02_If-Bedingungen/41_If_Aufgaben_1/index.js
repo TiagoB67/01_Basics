ffaa96883793f84f255f19f3efd1035f0a7cf0f8
TiagoB67 <tiagobiernath@gmail.com>
2025-11-19 16:14:56 +0100

    41_If_Aufgabe Else Done

---
+++ 02_If-Bedingungen/41_If_Aufgaben_1/index.js
@@ -0,0 +1,9 @@
+let a = 1;
+if (a >= 0){ console.log("The number is positive")}
+else {console.log("The Number is not positive")}
+
+if (a <= 0){console.log("The Number is negative")}
+else {console.log("The number is not negative")}
+
+if (a == 0){console.log("The number is zero")}
+else {console.log("The number is not 0")}
