a40ba016a0cbae996909e50e10f7d8496e113093
TiagoB67 <tiagobiernath@gmail.com>
2025-11-19 15:42:08 +0100

    40_If-Bedingungen_Aufgabe_2 Done

---
+++ 02_If-Bedingungen/40_If-Bedingungen/40_Aufgabe_2/index.js
@@ -0,0 +1,5 @@
+let trafficLight = "green";
+
+if (trafficLight == "green"){console.log("You may go")}
+if (trafficLight == "yellow"){console.log("Caution!")}
+if (trafficLight == "red"){console.log("Stop!")}
