340cf73e9994f311e6890c9cdf46239c6778860b
TiagoB67 <tiagobiernath@gmail.com>
2025-12-03 16:14:07 +0100

    42_If-Bedingungen_Aufgabe_1 Done

---
+++ 02_If-Bedingungen/42_If-Bedingung If Else/index.js
@@ -0,0 +1,5 @@
+let randomNumber = Math.floor(Math.random()*10);
+
+if (randomNumber >= 5) {console.log("The number is greater than 5")}
+else if(randomNumber <= 5){console.log("The number is less than 5")}
+else{console.log("The number is exactly 5")};
