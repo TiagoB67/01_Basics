be8846a63d9ac23f53e4b0794c3a9a615bdf2562
TiagoB67 <tiagobiernath@gmail.com>
2025-12-17 16:30:33 +0100

    45_Deno_Aufgabe_2 Done

---
+++ 03_Advanced/45_Deno/Aufgabe_2_Number_Check/index.js
@@ -0,0 +1,18 @@
+let YourNumber = prompt("Please enter a number:");
+let number = parseInt(YourNumber);
+
+if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is positive or 0.");
+}
+if (number > 0) {
+  console.log("The number is positive.");
+} else {
+  console.log("The number is negative or 0.");
+}
+if (number === 0) {
+  console.log("The number is 0.");
+} else {
+  console.log("The number is not 0.");
+}
