3e83a5f4d654507135463b362c03acaea80dcd7e
TiagoB67 <tiagobiernath@gmail.com>
2025-12-30 19:59:27 +0100

    45_Deno_Aufgabe_6 Done

---
+++ 03_Advanced/45_Deno/Aufgabe_6_Getränkeautomat/index.js
@@ -0,0 +1,14 @@
+let Gertänk = prompt("Möchten Sie Cola, Wasser, Bier oder Tee kaufen?");
+
+//Getänk Auswählen
+if (Gertänk == "Cola") {
+  console.log("Danke für ihre Bestellung von Cola");
+} else if (Gertänk == "Wasser") {
+  console.log("Danke für ihre Bestellung von Wasser");
+} else if (Gertänk == "Bier") {
+  console.log("Danke für ihre Bestellung von Bier");
+} else if (Gertänk == "Tee") {
+  console.log("Danke für ihre Bestellung von Tee");
+} else {
+  console.log("Dieses Getränk ist nicht verfügbar.");
+}
