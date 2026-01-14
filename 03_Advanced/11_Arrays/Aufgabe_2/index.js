722c2cb6aad44f85f6c0290d9e32503592b7c3be
TiagoB67 <tiagobiernath@gmail.com>
2026-01-14 16:19:40 +0100

    11_Arrays_Aufgabe_2 Done

---
+++ 03_Advanced/11_Arrays/Aufgabe_2/index.js
@@ -0,0 +1,14 @@
+//Arrays erstellen
+let a = ["Susi", " Paula", " Hans"];
+
+//Array ausgeben
+console.log("Meine Freunde sind " + a[0] + "," + a[1] + " und" + a[2]);
+
+//Array Werte Hinzufügen
+
+a.push("Sepp");
+
+//Neuen Array Ausgeben
+console.log(
+  "Meine Freunde sind" + a[0] + "," + a[1] + "," + a[2] + " und " + a[3]
+);
